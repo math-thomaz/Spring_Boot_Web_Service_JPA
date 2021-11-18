928230d86b0171f3c12787228c60e0e1ac895f14
Matheus Thomaz <matheustthomaz@gmail.com>
2021-11-18 16:42:52 -0300

    H2 database, test profile, JPA

--- src/main/java/com/maththomaz/webService/entities/User.java
+++ src/main/java/com/maththomaz/webService/entities/User.java
@@ -3,6 +3,8 @@ package com.maththomaz.webService.entities;
 import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
 
@@ -15,6 +17,7 @@ public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
