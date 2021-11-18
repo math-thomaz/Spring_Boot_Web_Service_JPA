9e93284312652c5257f537e3e1a4aa44e114d388
Matheus Thomaz <matheustthomaz@gmail.com>
2021-11-17 23:53:58 -0300

    Project created

---
+++ src/main/java/com/maththomaz/webService/SpringBootWebServiceJpaApplication.java
@@ -0,0 +1,13 @@
+package com.maththomaz.webService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebServiceJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebServiceJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maththomaz/webService/SpringBootWebServiceJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maththomaz.webService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebServiceJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
