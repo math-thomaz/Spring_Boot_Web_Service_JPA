fe09f8f46ea5bc0b6bf5955b3f30d812deee816a
Matheus Thomaz <matheustthomaz@gmail.com>
2021-11-24 22:26:21 -0300

    User delete resource

--- src/main/java/com/maththomaz/webService/resources/UserResource.java
+++ src/main/java/com/maththomaz/webService/resources/UserResource.java
@@ -36,4 +36,10 @@ public class UserResource {
                 .buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.remove(id);
+        return ResponseEntity.noContent().build();
+    }
 }
