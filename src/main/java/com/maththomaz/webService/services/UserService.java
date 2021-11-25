ccabcb6a365cd9b4b2b8cb5fe06476d1047ce6b2
Matheus Thomaz <matheustthomaz@gmail.com>
2021-11-25 17:11:17 -0300

    Exception handling - findById

--- src/main/java/com/maththomaz/webService/services/UserService.java
+++ src/main/java/com/maththomaz/webService/services/UserService.java
@@ -2,6 +2,7 @@ package com.maththomaz.webService.services;
 
 import com.maththomaz.webService.entities.User;
 import com.maththomaz.webService.repositories.UserRepository;
+import com.maththomaz.webService.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj =  userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
